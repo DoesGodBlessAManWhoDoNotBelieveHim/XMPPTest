05800e269dea912b294594747f6cf6f5f5e1cb7a
Jing Zhang <king91chang@163.com>
2015-08-24 12:06:16 +0800

    Initial Commit

---
+++ XMPPTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPTest
+//
+//  Created by wrt on 15/8/24.
+//  Copyright (c) 2015年 wrtsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XMPPTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMPPTest
+//
+//  Created by wrt on 15/8/24.
+//  Copyright (c) 2015年 wrtsoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
